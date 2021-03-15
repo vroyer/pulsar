47247c477584608a7f996daea838735eb6a25a97
vroyer <vroyer@strapdata.com>
2021-03-15 23:16:17 +0100

    Use an Elasticsearch container for unit tests

--- pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
+++ pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
@@ -21,39 +21,20 @@ package org.apache.pulsar.io.elasticsearch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.impl.schema.KeyValueSchema;
 import org.apache.pulsar.client.impl.schema.generic.GenericJsonRecord;
+import org.apache.pulsar.common.schema.SchemaType;
 import org.apache.pulsar.functions.api.Record;
 import org.apache.pulsar.io.core.Sink;
 import org.apache.pulsar.io.core.SinkContext;
 import org.apache.pulsar.io.core.annotations.Connector;
 import org.apache.pulsar.io.core.annotations.IOType;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
-import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.*;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentType;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * The base abstract class for ElasticSearch sinks.
@@ -68,22 +49,27 @@ import java.util.Optional;
 @Slf4j
 public class ElasticSearchSink implements Sink<Object> {
 
-    private URL url;
-    private RestHighLevelClient client;
-    private CredentialsProvider credentialsProvider;
     private ElasticSearchConfig elasticSearchConfig;
+    private ElasticsearchClient elasticsearchClient;
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void open(Map<String, Object> config, SinkContext sinkContext) throws Exception {
         elasticSearchConfig = ElasticSearchConfig.load(config);
         elasticSearchConfig.validate();
-        createIndexIfNeeded();
+        elasticsearchClient = new ElasticsearchClient(elasticSearchConfig);
     }
 
     @Override
     public void close() throws Exception {
-        client.close();
+        if (elasticsearchClient != null)
+            elasticsearchClient.close();
+        elasticsearchClient = null;
+    }
+
+    @VisibleForTesting
+    void setElasticsearchClient(ElasticsearchClient elasticsearchClient) {
+        this.elasticsearchClient = elasticsearchClient;
     }
 
     @Override
@@ -92,54 +78,12 @@ public class ElasticSearchSink implements Sink<Object> {
             System.out.println("Writing record.value=" + record.getValue());
             Pair<String, String> idAndDoc = extractIdAndDocument(record);
             if(idAndDoc.getValue() == null) {
-                deleteDocument(record, idAndDoc);
+                elasticsearchClient.deleteDocument(record, idAndDoc);
             } else {
-                indexDocument(record, idAndDoc);
+                elasticsearchClient.indexDocument(record, idAndDoc);
             }
         } catch(Exception e) {
             log.error("Unexpected error", e);
-        }
-    }
-
-    void indexDocument(Record<Object> record, Pair<String, String> document) {
-        IndexRequest indexRequest = Requests.indexRequest(elasticSearchConfig.getIndexName());
-        if (document.getLeft() != null) {
-            indexRequest.id(document.getLeft());
-        }
-        indexRequest.type(elasticSearchConfig.getTypeName());
-        indexRequest.source(document.getRight(), XContentType.JSON);
-
-        try {
-            IndexResponse indexResponse = getClient().index(indexRequest, RequestOptions.DEFAULT);
-            System.out.println("index result=" + indexResponse.getResult());
-            if (indexResponse.getResult().equals(DocWriteResponse.Result.CREATED) ||
-                    indexResponse.getResult().equals(DocWriteResponse.Result.UPDATED)) {
-                record.ack();
-            } else {
-                record.fail();
-            }
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-            record.fail();
-        }
-    }
-
-    void deleteDocument(Record<Object> record, Pair<String, String> document) {
-        DeleteRequest deleteRequest = Requests.deleteRequest(elasticSearchConfig.getIndexName());
-        deleteRequest.id(document.getLeft());
-        deleteRequest.type(elasticSearchConfig.getTypeName());
-        try {
-            DeleteResponse deleteResponse = getClient().delete(deleteRequest, RequestOptions.DEFAULT);
-            System.out.println("delete result=" + deleteResponse.getResult());
-            if (deleteResponse.getResult().equals(DocWriteResponse.Result.DELETED) ||
-                    deleteResponse.getResult().equals(DocWriteResponse.Result.NOT_FOUND)) {
-                record.ack();
-            } else {
-                record.fail();
-            }
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-            record.fail();
         }
     }
 
@@ -221,65 +165,15 @@ public class ElasticSearchSink implements Sink<Object> {
             }
         }
 
-        System.out.println("recordType="+record.getClass().getName() +
-                        " schemaType="+record.getSchema().getSchemaInfo().getType() +
-                        " id=" + id +
-                        " doc=" + doc);
+        SchemaType schemaType = null;
+        if (record.getSchema() != null && record.getSchema().getSchemaInfo() != null) {
+            schemaType = record.getSchema().getSchemaInfo().getType();
+        }
+        log.debug("recordType="+ record.getClass().getName() +
+                " schemaType="+ schemaType +
+                " id=" + id +
+                " doc=" + doc);
         return Pair.of(id, doc);
     }
 
-    private void createIndexIfNeeded() throws IOException {
-        GetIndexRequest request = new GetIndexRequest();
-        request.indices(elasticSearchConfig.getIndexName());
-        boolean exists = getClient().indices().exists(request, RequestOptions.DEFAULT);
-
-        if (!exists) {
-            CreateIndexRequest cireq = new CreateIndexRequest(elasticSearchConfig.getIndexName());
-
-            cireq.settings(Settings.builder()
-               .put("index.number_of_shards", elasticSearchConfig.getIndexNumberOfShards())
-               .put("index.number_of_replicas", elasticSearchConfig.getIndexNumberOfReplicas()));
-
-            CreateIndexResponse ciresp = getClient().indices().create(cireq, RequestOptions.DEFAULT);
-            if (!ciresp.isAcknowledged() || !ciresp.isShardsAcknowledged()) {
-                throw new RuntimeException("Unable to create index.");
-            }
-        }
-    }
-
-    private URL getUrl() throws MalformedURLException {
-        if (url == null) {
-            url = new URL(elasticSearchConfig.getElasticSearchUrl());
-        }
-        return url;
-    }
-
-    private CredentialsProvider getCredentialsProvider() {
-
-        if (StringUtils.isEmpty(elasticSearchConfig.getUsername())
-            || StringUtils.isEmpty(elasticSearchConfig.getPassword())) {
-            return null;
-        }
-
-        credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(elasticSearchConfig.getUsername(),
-                        elasticSearchConfig.getPassword()));
-        return credentialsProvider;
-    }
-
-    private RestHighLevelClient getClient() throws MalformedURLException {
-        if (client == null) {
-          CredentialsProvider cp = getCredentialsProvider();
-          RestClientBuilder builder = RestClient.builder(new HttpHost(getUrl().getHost(),
-                  getUrl().getPort(), getUrl().getProtocol()));
-
-          if (cp != null) {
-              builder.setHttpClientConfigCallback(httpClientBuilder ->
-              httpClientBuilder.setDefaultCredentialsProvider(cp));
-          }
-          client = new RestHighLevelClient(builder);
-        }
-        return client;
-    }
 }
--- pulsar-io/elastic-search/src/test/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSinkTests.java
+++ pulsar-io/elastic-search/src/test/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSinkTests.java
@@ -21,14 +21,20 @@ package org.apache.pulsar.io.elasticsearch;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.andreinc.mockneat.MockNeat;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.common.schema.KeyValue;
+import org.apache.pulsar.common.schema.KeyValueEncodingType;
 import org.apache.pulsar.functions.api.Record;
 import org.apache.pulsar.io.core.SinkContext;
 import org.apache.pulsar.io.elasticsearch.data.Profile;
 import org.apache.pulsar.io.elasticsearch.data.UserProfile;
 import org.elasticsearch.ElasticsearchStatusException;
+import org.junit.AfterClass;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -42,8 +48,11 @@ import static org.mockito.Mockito.*;
 
 public class ElasticSearchSinkTests {
 
+    public final static String ELASTICSEARCH_IMAGE = "docker.elastic.co/elasticsearch/elasticsearch-oss:7.10.2-amd64";
+
     protected static MockNeat mockNeat;
     protected static Gson gson;
+    private static ElasticsearchContainer container;
 
     @Mock
     protected Record<Object> mockRecord;
@@ -54,18 +63,26 @@ public class ElasticSearchSinkTests {
     protected ElasticSearchSink sink;
 
     @BeforeClass
-    public static final void init() {
+    public static final void initBeforeClass() {
         mockNeat = MockNeat.threadLocal();
         gson = new GsonBuilder()
                 .setPrettyPrinting()
                 .create();
+        container = new ElasticsearchContainer(ELASTICSEARCH_IMAGE);
+    }
+
+    @AfterClass
+    public static void closeAfterClass() {
+        container.close();
     }
 
     @SuppressWarnings("unchecked")
     @BeforeMethod
     public final void setUp() throws Exception {
+        container.start();
+
         map = new HashMap<String, Object> ();
-        map.put("elasticSearchUrl", "http://localhost:9200");
+        map.put("elasticSearchUrl", "http://"+container.getHttpHostAddress());
         sink = new ElasticSearchSink();
 
         mockRecord = mock(Record.class);
@@ -77,9 +94,14 @@ public class ElasticSearchSinkTests {
                return Optional.of( "key-" + sequenceCounter++);
             }});
 
-        when(mockRecord.getValue()).thenAnswer(new Answer<byte[]>() {
-            public byte[] answer(InvocationOnMock invocation) throws Throwable {
-                 return getJSON().getBytes();
+        when(mockRecord.getValue()).thenAnswer(new Answer<String>() {
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                 return getJSON();
+            }});
+
+        when(mockRecord.getKeyValueSchema()).thenAnswer(new Answer<Schema<KeyValue<String,String>>>() {
+            public Schema<KeyValue<String,String>> answer(InvocationOnMock invocation) throws Throwable {
+                return Schema.KeyValue(Schema.STRING, Schema.STRING, KeyValueEncodingType.SEPARATED);
             }});
     }
 
@@ -92,15 +114,17 @@ public class ElasticSearchSinkTests {
     public final void invalidIndexNameTest() throws Exception {
         map.put("indexName", "myIndex");
         sink.open(map, mockSinkContext);
+        send(1);
     }
 
     @Test(enabled = true)
     public final void createIndexTest() throws Exception {
         map.put("indexName", "test-index");
         sink.open(map, mockSinkContext);
+        send(1);
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public final void singleRecordTest() throws Exception {
         map.put("indexName", "test-index");
         sink.open(map, mockSinkContext);
@@ -108,7 +132,7 @@ public class ElasticSearchSinkTests {
         verify(mockRecord, times(1)).ack();
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public final void send100Test() throws Exception {
         map.put("indexName", "test-index");
         sink.open(map, mockSinkContext);
