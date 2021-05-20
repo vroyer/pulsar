545edcb74129ddb1dc5c42f68cb3763bf608dda5
Enrico Olivelli <eolivelli@apache.org>
2021-05-20 11:42:07 +0200

    Enhanced ES-sink connector

--- pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
+++ pulsar-io/elastic-search/src/main/java/org/apache/pulsar/io/elasticsearch/ElasticSearchSink.java
@@ -18,139 +18,243 @@
  */
 package org.apache.pulsar.io.elasticsearch;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.schema.GenericObject;
+import org.apache.pulsar.client.api.schema.GenericRecord;
+import org.apache.pulsar.client.impl.schema.KeyValueSchema;
+import org.apache.pulsar.common.schema.KeyValue;
+import org.apache.pulsar.common.schema.SchemaType;
 import org.apache.pulsar.functions.api.Record;
-import org.apache.pulsar.io.core.KeyValue;
 import org.apache.pulsar.io.core.Sink;
 import org.apache.pulsar.io.core.SinkContext;
 import org.apache.pulsar.io.core.annotations.Connector;
 import org.apache.pulsar.io.core.annotations.IOType;
-import org.elasticsearch.action.DocWriteResponse;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
-import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.*;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentType;
 
-/**
- * The base abstract class for ElasticSearch sinks.
- * Users need to implement extractKeyValue function to use this sink.
- * This class assumes that the input will be JSON documents
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 @Connector(
-    name = "elastic_search",
-    type = IOType.SINK,
-    help = "A sink connector that sends pulsar messages to elastic search",
-    configClass = ElasticSearchConfig.class
+        name = "elastic_search",
+        type = IOType.SINK,
+        help = "A sink connector that sends pulsar messages to elastic search",
+        configClass = ElasticSearchConfig.class
 )
-public class ElasticSearchSink implements Sink<byte[]> {
+@Slf4j
+public class ElasticSearchSink implements Sink<GenericObject> {
 
-    private URL url;
-    private RestHighLevelClient client;
-    private CredentialsProvider credentialsProvider;
     private ElasticSearchConfig elasticSearchConfig;
+    private ElasticSearchClient elasticsearchClient;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void open(Map<String, Object> config, SinkContext sinkContext) throws Exception {
         elasticSearchConfig = ElasticSearchConfig.load(config);
         elasticSearchConfig.validate();
-        createIndexIfNeeded();
+        elasticsearchClient = new ElasticSearchClient(elasticSearchConfig);
     }
 
     @Override
-    public void close() throws Exception {
-        client.close();
+    public void close() {
+        if (elasticsearchClient != null) {
+            elasticsearchClient.close();
+            elasticsearchClient = null;
+        }
     }
 
     @Override
-    public void write(Record<byte[]> record) {
-        KeyValue<String, byte[]> keyValue = extractKeyValue(record);
-        IndexRequest indexRequest = Requests.indexRequest(elasticSearchConfig.getIndexName());
-        indexRequest.type(elasticSearchConfig.getTypeName());
-        indexRequest.source(keyValue.getValue(), XContentType.JSON);
+    public void write(Record<GenericObject> record) throws Exception {
+        if (!elasticsearchClient.isFailed()) {
+            try {
+                Pair<String, String> idAndDoc = extractIdAndDocument(record);
+                if (idAndDoc.getRight() == null) {
+                    switch (elasticSearchConfig.getNullValueAction()) {
+                        case DELETE:
+                            if (elasticSearchConfig.isBulkEnabled()) {
+                                elasticsearchClient.bulkDelete(record, idAndDoc.getLeft());
+                            } else {
+                                elasticsearchClient.deleteDocument(record, idAndDoc.getLeft());
+                            }
+                            break;
+                        case IGNORE:
+                            break;
+                        case FAIL:
+                            elasticsearchClient.failed(new PulsarClientException.InvalidMessageException("Unexpected null message value"));
+                            throw elasticsearchClient.irrecoverableError.get();
+                    }
+                } else {
+                    if (elasticSearchConfig.isBulkEnabled()) {
+                        elasticsearchClient.bulkIndex(record, idAndDoc);
+                    } else {
+                        elasticsearchClient.indexDocument(record, idAndDoc);
+                    }
+                }
+            } catch(JsonProcessingException jsonProcessingException) {
+                switch(elasticSearchConfig.getMalformedDocAction()) {
+                    case IGNORE:
+                        break;
+                    case WARN:
+                        log.warn("Ignoring malformed document messageId={}",
+                                record.getMessage().map(Message::getMessageId).orElse(null),
+                                jsonProcessingException);
+                    case FAIL:
+                        log.error("Malformed document messageId={}",
+                                record.getMessage().map(Message::getMessageId).orElse(null),
+                                jsonProcessingException);
+                        elasticsearchClient.failed(jsonProcessingException);
+                        throw jsonProcessingException;
+                }
+            } catch (Exception e) {
+                log.error("write error:", e);
+                throw e;
+            }
+        }
+    }
 
-        try {
-        IndexResponse indexResponse = getClient().index(indexRequest, RequestOptions.DEFAULT);
-            if (indexResponse.getResult().equals(DocWriteResponse.Result.CREATED)) {
-                record.ack();
+    @VisibleForTesting
+    ElasticSearchClient getElasticsearchClient() {
+        return this.elasticsearchClient;
+    }
+
+    /**
+     * Extract ES _id and _source using the Schema if available.
+     *
+     * @param record
+     * @return A pair for _id and _source
+     */
+    public Pair<String, String> extractIdAndDocument(Record<GenericObject> record) throws JsonProcessingException {
+        Object key = null;
+        Object value = null;
+        Schema<?> keySchema = null;
+        Schema<?> valueSchema = null;
+
+        if (record.getSchema() instanceof KeyValueSchema) {
+            KeyValueSchema<GenericObject,GenericObject> keyValueSchema = (KeyValueSchema) record.getSchema();
+            keySchema = keyValueSchema.getKeySchema();
+            valueSchema = keyValueSchema.getValueSchema();
+            KeyValue keyValue = (KeyValue) record.getValue().getNativeObject();
+            key = keyValue.getKey();
+            value = keyValue.getValue();
+        } else {
+            key = record.getKey().orElse(null);
+            valueSchema = record.getSchema();
+            value = record.getValue() == null ? null : record.getValue().getNativeObject();
+        }
+
+        String id = null;
+        if (key != null) {
+            if (keySchema != null) {
+                id = stringifyKey(keySchema, key);
             } else {
-                record.fail();
-            }
-        } catch (final IOException ex) {
-            record.fail();
-        }
-    }
-
-    public KeyValue<String, byte[]> extractKeyValue(Record<byte[]> record) {
-        String key = record.getKey().orElse("");
-        return new KeyValue<>(key, record.getValue());
-    }
-
-    private void createIndexIfNeeded() throws IOException {
-        GetIndexRequest request = new GetIndexRequest();
-        request.indices(elasticSearchConfig.getIndexName());
-        boolean exists = getClient().indices().exists(request, RequestOptions.DEFAULT);
-
-        if (!exists) {
-            CreateIndexRequest cireq = new CreateIndexRequest(elasticSearchConfig.getIndexName());
-
-            cireq.settings(Settings.builder()
-               .put("index.number_of_shards", elasticSearchConfig.getIndexNumberOfShards())
-               .put("index.number_of_replicas", elasticSearchConfig.getIndexNumberOfReplicas()));
-
-            CreateIndexResponse ciresp = getClient().indices().create(cireq, RequestOptions.DEFAULT);
-            if (!ciresp.isAcknowledged() || !ciresp.isShardsAcknowledged()) {
-                throw new RuntimeException("Unable to create index.");
+                id = key.toString();
             }
         }
-    }
 
-    private URL getUrl() throws MalformedURLException {
-        if (url == null) {
-            url = new URL(elasticSearchConfig.getElasticSearchUrl());
-        }
-        return url;
-    }
-
-    private CredentialsProvider getCredentialsProvider() {
-
-        if (StringUtils.isEmpty(elasticSearchConfig.getUsername())
-            || StringUtils.isEmpty(elasticSearchConfig.getPassword())) {
-            return null;
+        String doc = null;
+        if (value != null) {
+            if (valueSchema != null) {
+                doc = stringifyValue(valueSchema, value);
+            } else {
+                doc = value.toString();
+            }
         }
 
-        credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(elasticSearchConfig.getUsername(),
-                        elasticSearchConfig.getPassword()));
-        return credentialsProvider;
+        if (elasticSearchConfig.isKeyIgnore()) {
+            if (doc == null || Strings.isNullOrEmpty(elasticSearchConfig.getPrimaryFields())) {
+                // use the messageId as the doc id in last resort.
+                id = Hex.encodeHexString(record.getMessage().get().getMessageId().toByteArray());
+            } else {
+                try {
+                    // extract the PK from the JSON document
+                    JsonNode jsonNode = objectMapper.readTree(doc);
+                    List<String> pkFields = Arrays.asList(elasticSearchConfig.getPrimaryFields().split(","));
+                    id = stringifyKey(jsonNode, pkFields);
+                } catch (JsonProcessingException e) {
+                    log.error("Failed to read JSON", e);
+                }
+            }
+        }
+
+        if (log.isDebugEnabled()) {
+            SchemaType schemaType = null;
+            if (record.getSchema() != null && record.getSchema().getSchemaInfo() != null) {
+                schemaType = record.getSchema().getSchemaInfo().getType();
+            }
+            log.debug("recordType={} schemaType={} id={} doc={}",
+                    record.getClass().getName(),
+                    schemaType,
+                    id,
+                    doc);
+        }
+        return Pair.of(id, doc);
     }
 
-    private RestHighLevelClient getClient() throws MalformedURLException {
-        if (client == null) {
-          CredentialsProvider cp = getCredentialsProvider();
-          RestClientBuilder builder = RestClient.builder(new HttpHost(getUrl().getHost(),
-                  getUrl().getPort(), getUrl().getProtocol()));
-
-          if (cp != null) {
-              builder.setHttpClientConfigCallback(httpClientBuilder ->
-              httpClientBuilder.setDefaultCredentialsProvider(cp));
-          }
-          client = new RestHighLevelClient(builder);
+    public String stringifyKey(Schema<?> schema, Object val) throws JsonProcessingException {
+        switch (schema.getSchemaInfo().getType()) {
+            case INT8:
+                return Byte.toString((Byte) val);
+            case INT16:
+                return Short.toString((Short) val);
+            case INT32:
+                return Integer.toString((Integer) val);
+            case INT64:
+                return Long.toString((Long) val);
+            case STRING:
+                return (String) val;
+            case JSON:
+            case AVRO:
+                return stringifyKey(extractJsonNode(schema, val));
+            default:
+                throw new UnsupportedOperationException("Unsupported key schemaType=" + schema.getSchemaInfo().getType());
         }
-        return client;
+    }
+
+    /**
+     * Convert a JsonNode to an Elasticsearch id.
+     */
+    public String stringifyKey(JsonNode jsonNode) throws JsonProcessingException {
+        List<String> fields = new ArrayList<>();
+        jsonNode.fieldNames().forEachRemaining(fields::add);
+        return stringifyKey(jsonNode, fields);
+    }
+
+    public String stringifyKey(JsonNode jsonNode, List<String> fields) throws JsonProcessingException {
+        if (fields.size() == 1) {
+            JsonNode singleNode = jsonNode.get(fields.get(0));
+            String id = objectMapper.writeValueAsString(singleNode);
+            return (id.startsWith("\"") && id.endsWith("\""))
+                    ? id.substring(1, id.length() - 1)  // remove double quotes
+                    : id;
+        } else {
+            return JsonConverter.toJsonArray(jsonNode, fields).toString();
+        }
+    }
+
+    public String stringifyValue(Schema<?> schema, Object val) throws JsonProcessingException {
+        JsonNode jsonNode = extractJsonNode(schema, val);
+        return objectMapper.writeValueAsString(jsonNode);
+    }
+
+    public JsonNode extractJsonNode(Schema<?> schema, Object val) {
+        switch (schema.getSchemaInfo().getType()) {
+            case JSON:
+                return (JsonNode) ((GenericRecord) val).getNativeObject();
+            case AVRO:
+                org.apache.avro.generic.GenericRecord node = (org.apache.avro.generic.GenericRecord) ((GenericRecord) val).getNativeObject();
+                return JsonConverter.toJson(node);
+            default:
+                throw new UnsupportedOperationException("Unsupported value schemaType=" + schema.getSchemaInfo().getType());
+        }
     }
 }
